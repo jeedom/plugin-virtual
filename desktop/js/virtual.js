b1312858b7bc2c916480567b87318dc08181fd34
Loïc <loic@jeedom.com>
2016-08-30 15:58:16 +0200

    improve UI

--- desktop/js/virtual.js
+++ desktop/js/virtual.js
@@ -112,7 +112,7 @@
        tr += '<td>';
   tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label></span> ';
     tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label></span> ';
-    tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr expertModeVisible" data-l1key="display" data-l2key="invertBinary"/>{{Inverser}}</label></span> ';
+    tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr expertModeVisible" data-l1key="display" data-l2key="invertBinary"/>{{Inverser}}</label></span><br/>';
        tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" style="width : 40%;display : inline-block;"> ';
        tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" style="width : 40%;display : inline-block;">';
        tr += '</td>';
--- desktop/php/virtual.php
+++ desktop/php/virtual.php
@@ -27,12 +27,12 @@ foreach ($eqLogics as $eqLogic) {
     </legend>
     <div class="eqLogicThumbnailContainer">
       <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-       <center>
-        <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-    </center>
-    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-</div>
-<?php
+         <center>
+            <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+    </div>
+    <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
@@ -47,7 +47,7 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
- <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+   <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
    <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
 
    <ul class="nav nav-tabs" role="tablist">
@@ -57,38 +57,38 @@ foreach ($eqLogics as $eqLogic) {
 
 <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
     <div role="tabpanel" class="tab-pane active" id="eqlogictab">
-    <form class="form-horizontal">
-        <fieldset>
-            <legend>
-                <i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
-                <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
-                <a class="btn btn-xs btn-default pull-right eqLogicAction" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
-                <a class="btn btn-xs btn-default pull-right" id="bt_importEqLogic"><i class="fa fa-share"></i> {{Importer équipement}}</a>
-            </legend>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">{{Nom de l'équipement virtual}}</label>
-                <div class="col-sm-3">
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement virtuel}}"/>
+        <form class="form-horizontal">
+            <fieldset>
+                <legend>
+                    <i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
+                    <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
+                    <a class="btn btn-xs btn-default pull-right eqLogicAction" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
+                    <a class="btn btn-xs btn-default pull-right" id="bt_importEqLogic"><i class="fa fa-share"></i> {{Importer équipement}}</a>
+                </legend>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">{{Nom de l'équipement virtual}}</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement virtuel}}"/>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" >{{Objet parent}}</label>
-                <div class="col-sm-3">
-                    <select class="form-control eqLogicAttr" data-l1key="object_id">
-                        <option value="">{{Aucun}}</option>
-                        <?php
+                <div class="form-group">
+                    <label class="col-sm-2 control-label" >{{Objet parent}}</label>
+                    <div class="col-sm-3">
+                        <select class="form-control eqLogicAttr" data-l1key="object_id">
+                            <option value="">{{Aucun}}</option>
+                            <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-                   </select>
+                       </select>
+                   </div>
                </div>
-           </div>
-           <div class="form-group">
-            <label class="col-sm-2 control-label">{{Catégorie}}</label>
-            <div class="col-sm-8">
-                <?php
+               <div class="form-group">
+                <label class="col-sm-2 control-label">{{Catégorie}}</label>
+                <div class="col-sm-8">
+                    <?php
 foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 	echo '<label class="checkbox-inline">';
 	echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
@@ -96,53 +96,53 @@ foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 }
 ?>
 
+               </div>
            </div>
-       </div>
-        <div class="form-group">
-            <label class="col-sm-2 control-label"></label>
-            <div class="col-sm-9">
+           <div class="form-group">
+              <label class="col-sm-2 control-label"></label>
+              <div class="col-sm-9">
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
             </div>
         </div>
-    <div class="form-group expertModeVisible">
-    <label class="col-sm-2 control-label">{{Auto-actualisation (cron)}}</label>
-        <div class="col-sm-2">
-            <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Auto-actualisation (cron)}}"/>
+        <div class="form-group expertModeVisible">
+            <label class="col-sm-2 control-label">{{Auto-actualisation (cron)}}</label>
+            <div class="col-sm-2">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Auto-actualisation (cron)}}"/>
+            </div>
+            <div class="col-sm-1">
+                <i class="fa fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
+            </div>
         </div>
-        <div class="col-sm-1">
-            <i class="fa fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">{{Commentaire}}</label>
+            <div class="col-sm-3">
+                <textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire" ></textarea>
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label class="col-sm-2 control-label">{{Commentaire}}</label>
-        <div class="col-sm-3">
-            <textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire" ></textarea>
-        </div>
-    </div>
-</fieldset>
+    </fieldset>
 </form>
 
 </div>
 <div role="tabpanel" class="tab-pane" id="commandtab">
-<a class="btn btn-default btn-sm pull-right" id="bt_addVirtualInfo"><i class="fa fa-plus-circle"></i> {{Ajouter une info virtuelle}}</a>
-<a class="btn btn-default btn-sm  pull-right" id="bt_addVirtualAction"><i class="fa fa-plus-circle"></i> {{Ajouter une commande virtuelle}}</a><br/><br/>
-<table id="table_cmd" class="table table-bordered table-condensed">
-    <thead>
-        <tr>
-            <th style="width: 50px;">#</th>
-            <th style="width: 230px;">{{Nom}}</th>
-            <th style="width: 110px;">{{Sous-Type}}</th>
-            <th>{{Valeur}}</th>
-            <th style="width: 100px;">{{Unité}}</th>
-            <th style="width: 200px;">{{Paramètres}}</th>
-            <th style="width: 100px;"></th>
-        </tr>
-    </thead>
-    <tbody>
+    <a class="btn btn-default btn-sm pull-right" id="bt_addVirtualInfo"><i class="fa fa-plus-circle"></i> {{Ajouter une info virtuelle}}</a>
+    <a class="btn btn-default btn-sm  pull-right" id="bt_addVirtualAction"><i class="fa fa-plus-circle"></i> {{Ajouter une commande virtuelle}}</a><br/><br/>
+    <table id="table_cmd" class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th style="width: 50px;">#</th>
+                <th style="width: 230px;">{{Nom}}</th>
+                <th style="width: 110px;">{{Sous-Type}}</th>
+                <th>{{Valeur}}</th>
+                <th style="width: 100px;">{{Unité}}</th>
+                <th style="width: 200px;">{{Paramètres}}</th>
+                <th style="width: 100px;"></th>
+            </tr>
+        </thead>
+        <tbody>
 
-    </tbody>
-</table>
+        </tbody>
+    </table>
 
 </div>
 </div>
