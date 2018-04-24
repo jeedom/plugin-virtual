a50afab14a49eeb9165a5dc2deb10b7ac3c15383
minscof <minscof@hotmail.com>
2018-04-24 15:51:33 +0200

    modify url to change value

--- desktop/php/virtual.php
+++ desktop/php/virtual.php
@@ -115,7 +115,7 @@ foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
       <div class="col-sm-9 callback">
         <span>
 	<?php
-echo network::getNetworkAccess('external') . '/core/api/jeeApi.php?plugin=virtual&apikey=' . jeedom::getApiKey($plugin->getId()) . '&type=virtual&id=#cmd_id#&value=#value#';
+echo network::getNetworkAccess('external') . '/core/api/jeeApi.php?type=virtual&apikey=' . jeedom::getApiKey($plugin->getId()) . '&id=#cmd_id#&value=#value#';
 ?>
 	</span>
       </div>
