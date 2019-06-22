61ac88e41e5b0cf4ee4d4ac83f51bf7abb95b7a1
jlayec <jerome@layec.com>
2019-06-22 16:14:56 +0200

    import improve
    import commands in a virtual with existing commands

--- core/class/virtual.class.php
+++ core/class/virtual.class.php
@@ -142,11 +142,21 @@ class virtual extends eqLogic {
 		foreach ($eqLogic->getCategory() as $key => $value) {
 			$this->setCategory($key, $value);
 		}
+		$arrayEquipmentCmd = $this->getCmd();
++               if (sizeof($arrayEquipmentCmd) > 1) {
++                       $virtual_with_commands = true;
++               } else {
++                       $virtual_with_commands = false;
++               }
 		foreach ($eqLogic->getCmd() as $cmd_def) {
 			$cmd_name = $cmd_def->getName();
 			if ($cmd_name == __('Rafraichir',__FILE__)) {
 				$cmd_name .= '_1';
 			}
+			if ($virtual_with_commands) {
++                               $cmd_name .= '_' . $cmd_def->getId();
++                       }
++                       log::add('virtual', 'debug', 'import equipement : ' . $eqLogic->getName() . ' > ajout de la commande : ' . $cmd_name);
 			$cmd = new virtualCmd();
 			$cmd->setName($cmd_name);
 			$cmd->setEqLogic_id($this->getId());
