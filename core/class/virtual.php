a77237be5dd6ae7d53aa28f16d890a5c3f64c4dc
Loïc <loic@jeedom.com>
2021-12-01 09:45:51 +0100

    Add files via upload

---
+++ core/class/virtual.php
@@ -0,0 +1,202 @@
+<?php
+if (!isConnect('admin')) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
+$plugin = plugin::byId('virtual');
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
+?>
+
+<div class="row row-overflow">
+	<div class="col-xs-12 eqLogicThumbnailDisplay">
+		<legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<div class="cursor eqLogicAction logoPrimary" data-action="add">
+				<i class="fas fa-plus-circle"></i>
+				<br>
+				<span>{{Ajouter}}</span>
+			</div>
+		</div>
+		<legend><i class="fas fa-clone"></i> {{Mes virtuels}}</legend>
+		<?php	if (count($eqLogics) == 0) {
+			echo '<br/><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement virtuel n\'est paramétré, cliquer sur "Ajouter" pour commencer}}</div>';
+		} else {
+			$content = '';
+			$content .= '<div class="input-group" style="margin:5px;">';
+			$content .= '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>';
+			$content .= '<div class="input-group-btn">';
+			$content .= '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
+			$content .= '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
+			$content .= '</div>';
+			$content .= '</div>';
+			$content .= '<div class="eqLogicThumbnailContainer">';
+			foreach ($eqLogics as $eqLogic) {
+				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+				$eqString = '';
+				$eqString .= '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+				$file = 'plugins/virtual/plugin_info/' . $eqLogic->getConfiguration('icon') . '.png';
+				if (file_exists(__DIR__.'/../../../../'.$file)) {
+					$eqString .= '<img src="'.$file.'" height="105" width="95" />';
+				}
+				else {
+					$eqString .= '<img src="' . $plugin->getPathImgIcon() . '"/>';
+				}
+				$eqString .= '<br>';
+				$eqString .= '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+
+				$eqString .= '<span class="hiddenAsCard displayTableRight">';
+				if ($eqLogic->getConfiguration('autorefresh') != '') $eqString .= '<span>' . $eqLogic->getConfiguration('autorefresh') . '</span>';
+				$cats = $eqLogic->getCategory();
+				unset($cats['default']);
+				$eqString .= '<span>' . implode(array_keys($cats, 1), ', ') . '</span>';
+				if ($eqLogic->getIsVisible() == 1) {
+					$eqString .= ' <i class="fas fa-eye"></i>';
+				} else {
+					$eqString .= ' <i class="fas fa-eye-slash"></i>';
+				}
+				$eqString .= '</span>';
+				$eqString .= '</div>';
+
+				$content .= $eqString;
+			}
+			echo $content.'</div>';
+		} ?>
+	</div>
+
+	<div class="col-xs-12 eqLogic" style="display:none;">
+		<div class="input-group pull-right" style="display:inline-flex">
+			<span class="input-group-btn">
+				<a class="btn btn-primary btn-sm roundedLeft" id="bt_showExpressionTest"><i class="fas fa-check"></i><span class="hidden-xs"> {{Expression}}</span>
+				</a><a class="btn btn-default btn-sm" id="bt_importTemplate"><i class="fas fa-cubes"></i><span class="hidden-xs"> {{Template}}</span>
+				</a><a class="btn btn-default btn-sm" id="bt_importEqLogic"><i class="fas fa-share"></i><span class="hidden-xs"> {{Importer équipement}}</span>
+				</a><a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
+				</a><a class="btn btn-sm btn-default eqLogicAction" data-action="copy"><i class="fas fa-copy"></i><span class="hidden-xs">  {{Dupliquer}}</span>
+				</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
+				</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+			</span>
+		</div>
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
+			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
+			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
+		</ul>
+		<div class="tab-content">
+			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="col-lg-6">
+							<legend><i class="fas fa-wrench"></i> {{Paramètres généraux}}</legend>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Nom du virtuel}}</label>
+								<div class="col-sm-7">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display:none;">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement virtuel}}">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label" >{{Objet parent}}</label>
+								<div class="col-sm-7">
+									<select class="form-control eqLogicAttr" data-l1key="object_id">
+										<option value="">{{Aucun}}</option>
+										<?php	$options = '';
+										foreach ((jeeObject::buildTree(null, false)) as $object) {
+											$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
+										}
+										echo $options;
+										?>
+									</select>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Catégorie}}</label>
+								<div class="col-sm-7">
+									<?php	foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+										echo '<label class="checkbox-inline">';
+										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '">' . $value['name'];
+										echo '</label>';
+									}	?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Options}}</label>
+								<div class="col-sm-7">
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+								</div>
+							</div>
+
+							<legend><i class="fas fa-cogs"></i> {{Paramètres spécifiques}}</legend>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Auto-actualisation}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Fréquence de rafraîchissement de l'équipement}}"></i></sup>
+								</label>
+								<div class="col-sm-7">
+									<div class="input-group">
+										<input type="text" class="eqLogicAttr form-control roundedLeft" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Cliquer sur ? pour afficher l'assistant cron}}">
+										<span class="input-group-btn">
+											<a id="bt_cronGenerator" class="btn btn-default cursor jeeHelper roundedRight" data-helper="cron" title="{{Assistant cron}}">
+												<i class="fas fa-question-circle"></i>
+											</a>
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{URL de retour}}</label>
+								<div class="col-sm-9 callback">
+									<span>
+										<?php	echo network::getNetworkAccess('external') . '/core/api/jeeApi.php?plugin=virtual&type=event&apikey=' . jeedom::getApiKey($plugin->getId()) . '&id=#cmd_id#&value=#value#';
+										?>
+									</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-lg-6">
+							<legend><i class="fas fa-info"></i> {{Informations}}</legend>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Description du virtuel}}</label>
+								<div class="col-sm-8">
+									<textarea class="form-control eqLogicAttr autogrow" data-l1key="comment"></textarea>
+								</div>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+				<hr>
+			</div>
+
+			<div role="tabpanel" class="tab-pane" id="commandtab">
+				<div class="input-group pull-right" style="display:inline-flex;margin-top:5px;">
+					<span class="input-group-btn">
+						<a class="btn btn-info btn-xs roundedLeft" id="bt_addVirtualInfo"><i class="fas fa-plus-circle"></i> {{Ajouter une info virtuelle}}
+						</a><a class="btn btn-warning btn-xs roundedRight" id="bt_addVirtualAction"><i class="fas fa-plus-circle"></i> {{Ajouter une action virtuelle}}</a>
+					</span>
+				</div>
+				<br><br>
+				<div class="table-responsive">
+					<table id="table_cmd" class="table table-bordered table-condensed">
+						<thead>
+							<tr>
+								<th style="min-width:50px;width:50px;"> ID</th>
+								<th style="min-width:220px;width:300px;">{{Nom}}</th>
+								<th style="width:130px;">{{Type}}</th>
+								<th style="min-width:250px;">{{Valeur}}</th>
+								<th style="min-width:150px;width:250px;">{{Paramètres}}</th>
+								<th style="min-width:220px;width:360px;">{{Options}}</th>
+								<th style="min-width:80px;width:200px;">{{Actions}}</th>
+							</tr>
+						</thead>
+						<tbody>
+						</tbody>
+					</table>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+<?php
+include_file('desktop', 'virtual', 'js', 'virtual');
+include_file('core', 'plugin.template', 'js');
+?>
