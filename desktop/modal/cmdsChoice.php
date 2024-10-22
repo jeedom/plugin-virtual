989fcb2230c25c3ce6789f64bbb86e8982c85522
Loïc <1536036+zoic21@users.noreply.github.com>
2024-10-22 14:40:17 +0200

    typo + bugfix

--- desktop/modal/cmdsChoice.php
+++ desktop/modal/cmdsChoice.php
@@ -20,13 +20,9 @@ if (!isConnect('admin')) {
     throw new Exception('{{401 - Accès non autorisé}}');
 }
 $plugin = plugin::byId('virtual');
-
-$eqLogicId = init(eqLogic); 
-$idOriginal = init(idOriginal);  
-
-
+$eqLogicId = init('eqLogic'); 
+$idOriginal = init('idOriginal');  
 ?>
-
 <span class='pull-right'>
 	<a class="btn btn-default pull-left" id="bt_selectAllInfos">{{Selectionner Commandes Infos}}</a>
 </span>
@@ -36,103 +32,68 @@ $idOriginal = init(idOriginal);
 <span class='pull-right'>
 	<a class="btn btn-success pull-right" id="bt_validateObjectlist"><i class="fas fa-check-circle"></i> {{Valider}}</a>
 </span>
-
-
-
 <br/><br/>
 <div id='div_Alert' style="display: none;"></div>
-
-<table id="tableVirtual"
-  data-toggle="table"
-  data-height="600"
-  data-pagination="true"
-  data-search="true">
-
-
+<table id="tableVirtual" data-toggle="table" data-height="600" data-pagination="true" data-search="true">
 	<thead>
     <tr>
-
-	          <th data-field="nomobjet">{{Nom Commande}}</th>
-	          <th data-field="typeobjet">{{Type}}</th>
-            <th data-field="value">{{Valeur}}</th>
-            <th></th>
+      <th data-field="nomobjet">{{Nom Commande}}</th>
+      <th data-field="typeobjet">{{Type}}</th>
+      <th data-field="value">{{Valeur}}</th>
+      <th></th>
     </tr>
 	</thead>
-
-
 <tbody>
-
   <?php
-
-if(is_object(eqLogic::byId($eqLogicId))){
-  $cmds = cmd::byEqLogicId($eqLogicId);
-  foreach($cmds as $cmd){
-    $cmdId = $cmd->getId();
-    $cmdName = $cmd->getName();
-    $cmdType = $cmd->getType();
-    $cmdSubType = $cmd->getSubType();
-    if($cmdType == 'info'){
-      $cmdValue = $cmd->execCmd();
-    }else{
-      $cmdValue = '';
+  if(is_object(eqLogic::byId($eqLogicId))){
+    $cmds = cmd::byEqLogicId($eqLogicId);
+    foreach($cmds as $cmd){
+      $cmdId = $cmd->getId();
+      $cmdName = $cmd->getName();
+      $cmdType = $cmd->getType();
+      $cmdSubType = $cmd->getSubType();
+      if($cmdType == 'info'){
+        $cmdValue = $cmd->execCmd();
+      }else{
+        $cmdValue = '';
+      }
+      $cmdIsVisible = $cmd->getIsVisible();
+      echo '<tr><td><span class="label label-info"  style="font-size : 1em;cursor:default;">' .  $cmdName . ' </span></td>';
+      echo '<td><span class="' . ($cmdType == 'action' ? 'label label-warning' : 'label label-info') . '" style="font-size : 1em;cursor:default;">' . $cmdType . ' </span></td>';
+      echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' .  $cmdValue . ' </span></td>';
+      echo '<td><input type="checkbox" class="' . ($cmdType == 'action' ? 'checkAction' : 'checkInfos') . '"  data-idCmd="'.$cmdId.'" style="font-size: 1em; cursor: default;"></td></tr>';
     }
-   
-    $cmdIsVisible = $cmd->getIsVisible();
-  echo '<tr><td><span class="label label-info"  style="font-size : 1em;cursor:default;">' .  $cmdName . ' </span></td>';
-  echo '<td><span class="' . ($cmdType == 'action' ? 'label label-warning' : 'label label-info') . '" style="font-size : 1em;cursor:default;">' . $cmdType . ' </span></td>';
-  echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' .  $cmdValue . ' </span></td>';
-  echo '<td><input type="checkbox" class="' . ($cmdType == 'action' ? 'checkAction' : 'checkInfos') . '"  data-idCmd="'.$cmdId.'" style="font-size: 1em; cursor: default;"></td></tr>';
-
- 
-
   }
- 
-  
-}
-
- 
-
-
-
-   ?>
+  ?>
 </tbody>
 
 <script>
-
-
-    function updateTableFilter(check) {
-     var searchText = $('.search-input').val().toLowerCase();
-     $('#tableVirtual tbody tr').each(function() {
-        var $row = $(this);
-        var $nomobjet = $row.find('td:eq(0)').text().toLowerCase();
-        var $checkbox = $row.find('input[type="checkbox"].' + check);
-        if ($nomobjet.includes(searchText)) {
-          if( $checkbox.prop('checked')){
-               $checkbox.not(this).prop('checked', false);
-              }else{
-                $checkbox.not(this).prop('checked', true);
-              }
-          
-        } else {
-          $checkbox.not(this).prop('checked', false);
-        }
+  function updateTableFilter(check) {
+    var searchText = $('.search-input').val().toLowerCase();
+    $('#tableVirtual tbody tr').each(function() {
+      var $row = $(this);
+      var $nomobjet = $row.find('td:eq(0)').text().toLowerCase();
+      var $checkbox = $row.find('input[type="checkbox"].' + check);
+      if ($nomobjet.includes(searchText)) {
+        if( $checkbox.prop('checked')){
+              $checkbox.not(this).prop('checked', false);
+            }else{
+              $checkbox.not(this).prop('checked', true);
+            }
+        
+      } else {
+        $checkbox.not(this).prop('checked', false);
+      }
     });
-
-    }
-
-
+  }
 
  $('#bt_selectAllInfos').on('click',function(){
       updateTableFilter('checkInfos');
-
 });
 
 $('#bt_selectAllActions').on('click',function(){
       updateTableFilter('checkAction');
-
 });
-
-
 
 document.getElementById('bt_validateObjectlist').addEventListener('click', function() {
   var checkedCheckboxes = [];
@@ -171,12 +132,7 @@ document.getElementById('bt_validateObjectlist').addEventListener('click', funct
     $('#div_Alert').showAlert({ message: 'Aucune commande sélectionnée', level: 'danger' })
   }
 });
-
-
-
-
 </script>
-
 <?php include_file('desktop', 'boot_table', 'css', 'virtual');?>
 <?php include_file('desktop', 'boot_table', 'js', 'virtual');?>
 <?php include_file('desktop', 'virtual', 'js', 'virtual');?>
